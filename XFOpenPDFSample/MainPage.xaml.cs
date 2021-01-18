6b71d11ee5ec2077059aff3ed0f7c6cf6d0248a6
Gerald Versluis <gerald@verslu.is>
2021-01-18 13:55:02 +0100

    Implement PDF Viewing

--- XFOpenPDFSample/MainPage.xaml.cs
+++ XFOpenPDFSample/MainPage.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.XamarinFormsSaveOpenPDFPackage;
 using Xamarin.Forms;
 
 namespace XFOpenPDFSample
@@ -13,6 +16,19 @@ namespace XFOpenPDFSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            var stream = await httpClient.GetStreamAsync("https://gerald.verslu.is/subscribe.pdf");
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await stream.CopyToAsync(memoryStream);
+
+                await CrossXamarinFormsSaveOpenPDFPackage.Current.SaveAndView("myFile.pdf", "application/pdf", memoryStream, PDFOpenContext.InApp);
+            }
         }
     }
 }
